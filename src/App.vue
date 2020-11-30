58a67e4500e46da8fc5e4b079243cb1b5e56ba3f
alkoxx <jonjon.a@gmail.com>
2020-11-30 23:19:30 +0100

    gmap-vue added, axios added, started with form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GmapMap
+      :center="{lat:43.0, lng:-2.0}"
+      :zoom="5"
+      map-type-id="terrain"
+      style="position:absolute; top:0; left:0; bottom:0; width:100%; z-index:-1; "
+      @click="mensaje($event)"
+    >
+
+      <gmap-marker :position="{lat: 43.0,lng: -2.0}" :clickable="true" :draggable="true" @click="mensaje()"></gmap-marker>
+
+      <template v-slot:visible>
+        <div style="position:absolute; top:0; left:0; width:400px; height:100vh; background:rgba(0, 0, 0, 0.3); padding:10px">
+          
+          <form enctype="multipart/form-data">
+            <input type="text" placeholder="Latitude" v-model="lat">
+            <input type="text" placeholder="Longitude" v-model="lng">
+            <button @click.prevent="submit">Añadir imagen</button>
+          </form>
+          
+        </div>      
+      </template>    
+    </GmapMap>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      form: {
+        lat: '',
+        lng: ''
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

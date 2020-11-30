641f2523f15f8c4ba836715d84fd80ea54d7a4ff
alkoxx <jonjon.a@gmail.com>
2020-12-01 00:00:17 +0100

    Add click events, create markers with click coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <GmapMap
-      :center="{lat:43.0, lng:-2.0}"
+      :center="center"
       :zoom="5"
       map-type-id="terrain"
       style="position:absolute; top:0; left:0; bottom:0; width:100%; z-index:-1; "
-      @click="mensaje($event)"
+      @click="loadCoord($event)"
     >
 
-      <gmap-marker :position="{lat: 43.0,lng: -2.0}" :clickable="true" :draggable="true" @click="mensaje()"></gmap-marker>
+      <gmap-marker v-for="(m, index) in markers" :key="index" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position"></gmap-marker>
 
       <template v-slot:visible>
         <div style="position:absolute; top:0; left:0; width:400px; height:100vh; background:rgba(0, 0, 0, 0.3); padding:10px">
           
           <form enctype="multipart/form-data">
-            <input type="text" placeholder="Latitude" v-model="lat">
-            <input type="text" placeholder="Longitude" v-model="lng">
-            <button @click.prevent="submit">Añadir imagen</button>
+            <input type="text" placeholder="Latitude" v-model="form.lat">
+            <input type="text" placeholder="Longitude" v-model="form.lng">
+            <button @click.prevent="submitMarker">Add Marker</button>
           </form>
           
         </div>      
@@ -32,12 +32,25 @@ export default {
   name: 'App',
   data(){
     return {
+      center: {lat:43.0, lng:-2.0},
+      markers: [],
       form: {
         lat: '',
         lng: ''
       }
     }
+  },
+  methods: {
+    loadCoord(event){
+      this.form.lat = event.latLng.lat()
+      this.form.lng = event.latLng.lng()
+    },
+    submitMarker(){
+      let position = {position: {lat:this.form.lat, lng:this.form.lng}}
+      this.markers.push(position)
+    }
   }
+    
 }
 </script>
 

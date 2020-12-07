9237c7b5064c894176eed2e019a2b6e52b5b4dce
alkoxx <jonjon.a@gmail.com>
2020-12-07 16:27:06 +0100

    Added info-window for the markers

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@
     <GmapMap
       :center="center"
       :zoom="5"
-      map-type-id="terrain"
+      map-type-id="hybrid"
       style="position:absolute; top:0; left:0; bottom:0; width:100%; z-index:-1; "
       @click="loadCoord($event)"
     >
 
-      <gmap-marker v-for="(m, index) in markers" :key="index" 
-        :position="m.position" :clickable="true" :draggable="true"
-        @click="center=m.position"></gmap-marker>
+      <gmap-marker v-for="(marker, index) in markers" :key="index" 
+        :position="marker.position" :clickable="true" :draggable="true"
+        @click="onMarkerSelected(marker)"></gmap-marker>
 
+      <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" 
+        @closeclick="infoWinOpen=false">        
+      </gmap-info-window>
+
+      <gmap-marker :key="i" v-for="(m,i) in markers" :position="m.position" :clickable="true" @click="toggleInfoWindow(m,i)"></gmap-marker>
+    
       <template v-slot:visible>
         <div style="position:absolute; top:0; left:0; width:400px; height:100vh; background:rgba(0, 0, 0, 0.3); padding:10px">
           
@@ -22,6 +28,8 @@
 
             <button @click.prevent="submitMarker">Add Marker</button>
           </form>
+
+          <img v-if="imgUrl" :src="imgUrl" alt="" width="100%" height="300">
           
         </div>      
       </template>    
@@ -31,7 +39,10 @@
 </template>
 
 <script>
-import axios from 'axios'
+import { fetchMarkers } from './services/markers-services'
+import { addMarker } from './services/markers-services'
+
+const serverUrl = 'http://127.0.0.1:8000'
 
 export default {
   name: 'App',
@@ -41,34 +52,56 @@ export default {
       markers: [],
       form: {
         lat: '',
-        lng: ''
+        lng: '',
+        selectedFile: ''
       },
-      selectedFile: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+      imgUrl: '',
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMarkerId: null,
+      infoOptions: {
+        content: '',
+        //optional: offset infowindow so it visually sits nicely on top of our marker
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      },
     }
+  },
+  created(){
+      this.getMarkers();
   },
   methods: {
     loadCoord(event){
       this.form.lat = event.latLng.lat()
       this.form.lng = event.latLng.lng()
     },
-    async submitMarker(){
-      const fd = new FormData();
-      fd.append('image', this.selectedFile)
-      fd.append('lat', this.lat)
-      fd.append('lng', this.lng)
-      try{
-        await axios.post('http://127.0.0.1:8000/markers/add-marker', fd);
-      }catch(error){
-        console.log(error)
-      }
-      
-      /*
-      let markerData = {position: {lat:this.form.lat, lng:this.form.lng}, icon: this.img}
-      this.markers.push(markerData)
-      */
+    async getMarkers(){
+      this.markers = await fetchMarkers(serverUrl + '/api/markers')
+    },
+    submitMarker(){
+      addMarker(serverUrl + '/marker/add-marker', this.form)
+      this.getMarkers()
     },
     onFileSelected(event){
-      this.selectedFile = event.target.files[0];
+      this.form.selectedFile = event.target.files[0];
+    },
+    onMarkerSelected(marker){
+      console.log(marker.position)
+      //this.center=marker.position
+      //  this.toggleInfoWindow(marker)
+    },  
+    toggleInfoWindow(marker) {
+      this.infoWindowPos = marker.position;
+      this.infoOptions.content = '<div> Hola Hola Hola!!</div>';
+
+      if (this.currentMarkerId == marker.id) {
+        this.infoWinOpen = !this.infoWinOpen;
+      } else {
+        this.infoWinOpen = true;
+        this.currentMarkerId = marker.id;
+      }
     }
   }
     

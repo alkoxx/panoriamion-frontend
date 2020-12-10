a40dbe48cb17e67cdf48df08d4def5fb38afb4a8
alkoxx <jonjon.a@gmail.com>
2020-12-10 18:50:59 +0100

    fixing sidepanel

--- src/components/side-panel.vue
+++ src/components/side-panel.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
         <transition name="fade">
-            <div v-if="open" class="sidebar">kokoko</div>
+            <div>
+                <v-card v-if="open" class="sidepanel" dark>
+                    <v-card-title>
+                        <h1>Create Marker</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="Latitude" v-model="coords.lat" />
+                            <v-text-field label="Longitude" v-model="coords.lng" />
+                            <v-file-input accept="image/*" label="File input" @change="onFileSelected"></v-file-input>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn @click.prevent="submitMarker" color="success">Add</v-btn>                        
+                    </v-card-actions>
+                </v-card>
+            </div>
         </transition>
             
-        <div style="position:absolute; top:0; left:0; width:400px; height:100vh; background:rgba(0, 0, 0, 0.3); padding:10px">
-          
-          <form enctype="multipart/form-data">
-            <input type="text" placeholder="Latitude" v-model="form.lat">
-            <input type="text" placeholder="Longitude" v-model="form.lng">
-            <input type="file" @change="onFileSelected" />
-
-            <button @click.prevent="submitMarker">Add Marker</button>
-          </form>
-
-          <img v-if="imgUrl" :src="imgUrl" alt="" width="100%" height="300">
-          
-        </div>      
-        
         <div style="border:1px solid red; position:relative">aaa
             <v-btn @click="toggleSidePanel">Toggle</v-btn>
         </div>
@@ -53,27 +55,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
-.sidebar {
-    position: relative;
-    height: calc(100vh - 50px);
-    background-color: rgba(0,0,0,0.5);
-    width:300px;
-}
+    .sidepanel {  
+        position: relative;
+        height: calc(100vh - 50px);
+        padding-top: 48px;
+        max-width: 400px;
+        background-color: black;
+        //background-color: rgba($color: #242424, $alpha: .6);
+    }
 
-.fade-enter{
-    opacity: 0;
-}
+    .fade-enter{
+        opacity: 0;
+    }
 
-.fade-enter-active,
-.fade-leave-active{
-    transform: opacity 4s ease-in;
-}
+    .fade-enter-active,
+    .fade-leave-active{
+        transform: opacity 4s;
+    }
 
-.fade-leave-to{
-    opacity: 0;
-}
+    .fade-leave-to{
+        opacity: 0;
+    }
 
 
 </style>

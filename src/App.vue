97a30c5ed58306d5a01f951ca0967fcac2581ad7
alkoxx <jonjon.a@gmail.com>
2020-12-16 00:58:48 +0100

    Refactoring and add modal dialog to show images

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,79 @@
 <template>
   <div id="app">
-    <gmap-map
-      ref="gmap"
-      :center="center"
-      :zoom="5"
-      map-type-id="hybrid"
-      style="position:absolute; top:0; left:0; bottom:0; width:100%; z-index:-1;"
-      @click="loadCoord($event)"
-    >
+    <v-app>
+      <gmap-map
+        ref="gmap"
+        :center="center"
+        :zoom="5"
+        map-type-id="hybrid"
+        class="gmap"
+        @click="loadCoord($event)"
+      >
+        <gmap-marker
+          v-for="(marker, i) in markers"
+          :key="i"
+          :position="marker.position"
+          :clickable="true"
+          @click="onMarkerSelected(marker)"
+        ></gmap-marker>
 
-      <gmap-marker v-for="(marker, index) in markers" :key="index" 
-        :position="marker.position" :clickable="true"
-        @click="onMarkerSelected(marker)"></gmap-marker>
+        <gmap-info-window
+          :options="infoOptions"
+          :position="infoWindowPos"
+          :opened="infoWinOpen"
+          @closeclick="infoWinOpen = false"
+        >
+          <info-card v-if="selectedMarker" :marker="selectedMarker"></info-card>
+        </gmap-info-window>
 
-      <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" 
-        @closeclick="infoWinOpen=false">
-        <info-card v-if="selectedMarker" :marker="selectedMarker"></info-card>
-      </gmap-info-window>
-
-      <template v-slot:visible>
-        <side-panel :coords="coords"></side-panel>
-      </template>
-    </gmap-map>
-
+        <template v-slot:visible>
+          <v-row>
+            <v-col cols="2">
+              <side-panel :coords="coords"></side-panel>
+              <v-btn @click.stop="dialog = !dialog">Dialog!!</v-btn>
+            </v-col>
+            <v-col cols="10">
+              <v-dialog v-model="dialog">
+                <v-card>
+                  <v-toolbar dark color="primary">
+                    <v-btn icon dark @click="dialog = false">
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title>Settings</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                      <v-btn dark text @click="dialog = false">
+                        Save
+                      </v-btn>
+                    </v-toolbar-items>
+                  </v-toolbar>
+                </v-card>
+              </v-dialog>
+            </v-col>
+          </v-row>
+        </template>
+      </gmap-map>
+    </v-app>
   </div>
 </template>
 
 <script>
-import infoCard from './components/info-card'
-import sidePanel from './components/side-panel'
-import {mapState} from 'vuex'
+import infoCard from "./components/info-card";
+import sidePanel from "./components/side-panel";
+import { mapState } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     infoCard,
     sidePanel,
   },
-  data(){
+  data() {
     return {
-      center: {lat:43.0, lng:-2.0},
+      center: { lat: 43.0, lng: -2.0 },
       coords: {
-        lat: '',
-        lng: ''
+        lat: "",
+        lng: "",
       },
       selectedMarker: null,
       infoWindowPos: null,
@@ -51,26 +82,27 @@ export default {
       infoOptions: {
         pixelOffset: {
           width: 0,
-          height: -35
-        }
+          height: -35,
+        },
       },
-    }
+      dialog: false,
+    };
   },
-  created(){
-    this.$store.dispatch('getMarkers')
+  created() {
+    this.$store.dispatch("getMarkers");
   },
   computed: {
-    ...mapState(['markers'])
+    ...mapState(["markers"]),
   },
   methods: {
-    loadCoord(event){
-      this.coords.lat = event.latLng.lat()
-      this.coords.lng = event.latLng.lng()
-    },    
-    onMarkerSelected(marker){
-      this.selectedMarker = marker
+    loadCoord(event) {
+      this.coords.lat = event.latLng.lat();
+      this.coords.lng = event.latLng.lng();
+    },
+    onMarkerSelected(marker) {
+      this.selectedMarker = marker;
       this.$refs.gmap.panTo(marker.position);
-      this.toggleInfoWindow(marker)
+      this.toggleInfoWindow(marker);
     },
     toggleInfoWindow(marker) {
       this.infoWindowPos = marker.position;
@@ -80,11 +112,21 @@ export default {
         this.infoWinOpen = true;
         this.currentMarkerId = marker.id;
       }
-    }
-  }
-    
-}
+    },
+  },
+};
 </script>
 
 <style>
+.container {
+  display: flex;
+}
+.gmap {
+  height: 100vh;
+}
+
+.carousel {
+  width: 500px;
+  height: 800px;
+}
 </style>
--- src/components/side-panel.vue
+++ src/components/side-panel.vue
@@ -1,97 +1,90 @@
 <template>
-    <div>
-        <transition name="fade">
-            <v-card v-if="open" class="sidepanel" dark>
-                <v-card-title>
-                    <h2>Create Marker</h2>
-                </v-card-title>
-                <v-card-text>
-                    <v-form>
-                        <v-text-field label="Latitude" v-model="coords.lat" />
-                        <v-text-field label="Longitude" v-model="coords.lng" />
-                        <v-file-input accept="image/*" label="File input" @change="onFileSelected"></v-file-input>
-                    </v-form>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn @click.prevent="submitMarker" color="success">Add</v-btn>                        
-                </v-card-actions>   
-            </v-card>
-        </transition>
-        
-        <v-btn @click="toggle">Toggle</v-btn>
-    </div>    
+  <div>
+    <!--
+    <label>
+      AutoComplete
+      <gmap-autocomplete
+        placeholder="This is a placeholder text"
+        @place_changed="setPlace"
+        >dsf asdfa ds
+      </gmap-autocomplete>
+    </label>
+    -->
+    <transition name="fade">
+      <v-card v-if="open" class="sidepanel" dark>
+        <v-card-title>
+          <h2>Create Marker</h2>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Latitude" v-model="coords.lat" />
+            <v-text-field label="Longitude" v-model="coords.lng" />
+            <v-file-input
+              accept="image/*"
+              label="File input"
+              @change="onFileSelected"
+            ></v-file-input>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click.prevent="submitMarker" color="success">Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </transition>
+
+    <v-btn @click="toggle">Toggle</v-btn>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'SidePanel',
-    props: {
-        coords: Object
+  name: "SidePanel",
+  props: {
+    coords: Object,
+  },
+  data() {
+    return {
+      open: true,
+      selectedFile: "",
+    };
+  },
+  methods: {
+    submitMarker() {
+      //addMarker(serverUrl + '/marker/add-marker', this.form)
+      this.getMarkers();
     },
-    data() {
-        return{
-            open: true,
-            selectedFile: ''
-        }
+    setPlace() {},
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
     },
-    methods: {
-        submitMarker(){
-            //addMarker(serverUrl + '/marker/add-marker', this.form)
-            this.getMarkers()
-        },
-        onFileSelected(event){
-            this.selectedFile = event.target.files[0];
-        },
-        toggleSidePanel(){
-            this.open = !this.open
-        },
-        toggle(){
-            this.open = !this.open
-        }
-    }
-
-}
+    toggleSidePanel() {
+      this.open = !this.open;
+    },
+    toggle() {
+      this.open = !this.open;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.sidepanel {
+  height: calc(100vh - 50px);
+  padding-top: 30px;
+  max-width: 400px;
+  background-color: rgba($color: #242424, $alpha: 0.6);
+}
 
-    .sidepanel {  
-        position: relative;
-        height: calc(100vh - 50px);
-        padding-top: 48px;
-        max-width: 400px;
-        background-color: black;
-        //background-color: rgba($color: #242424, $alpha: .6);
-    }
+.fade-enter {
+  opacity: 0;
+}
 
-    .fade-enter{
-        opacity: 0;
-    }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
 
-    .fade-enter-active,
-    .fade-leave-active{
-        transition: opacity 1s;
-    }
-
-    .fade-leave-to{
-        opacity: 0;
-    }
-
-    .test{
-        background-color: green;
-        height: 100px;
-        position:relative;
-    }
-
-    .tt-enter{
-        opacity: 0;
-    }
-    .tt-enter-active, .tt-leave-active{
-        transition: opacity 2s;
-    }
-    .tt-leave-to{
-        opacity: 0;
-    }
-
-
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
